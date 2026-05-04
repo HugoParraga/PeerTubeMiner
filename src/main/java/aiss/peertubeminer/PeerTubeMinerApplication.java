bad3ae1488fd7f91514d3def604ca73acd983c8a
Hugo Párraga <hugpardel@alum.us.es>
2026-05-04 19:54:44 +0200

    Prueba

--- src/main/java/aiss/peertubeminer/PeerTubeMinerApplication.java
+++ src/main/java/aiss/peertubeminer/PeerTubeMinerApplication.java
@@ -9,5 +9,5 @@ public class PeerTubeMinerApplication {
     public static void main(String[] args) {
         SpringApplication.run(PeerTubeMinerApplication.class, args);
     }
-    return ;
+    return hola;
 }
