bb38218519b9aa38f6e25d85c84d1480f6a006e8
Hugo Párraga <hugpardel@alum.us.es>
2026-05-04 20:19:15 +0200

    VideoMiner models created

---
+++ PeerTubeMiner/src/main/java/aiss/peertubeminer/model/videominer/Caption.java
@@ -0,0 +1,64 @@
+package aiss.peertubeminer.model.videominer;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Caption")
+public class    Caption {
+
+    @Id
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    @Column(name = "name")
+    private String name;
+
+    @JsonProperty("language")
+    @Column(name = "language")
+    private String language;
+
+    public Caption(){
+
+    }
+
+    public Caption(String id,String name, String language){
+        this.id=id;
+        this.name=name;
+        this.language=language;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Override
+    public String toString() {
+        return "Caption{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", language='" + language + '\'' +
+                '}';
+    }
+}
